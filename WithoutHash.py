bf707e0e56fd9956b92beb1b8d908e1db6404341
BTroy2021 <bridgettroy@hotmail.com>
2021-12-23 11:50:30 -0600

    Test

--- WithoutHash.py
+++ WithoutHash.py
@@ -8,7 +8,6 @@ import matplotlib.pyplot as plt
 
 fdi_csv = pd.read_csv(r'C:\Users\Bridg\Desktop\FDI.csv', encoding='utf-8')
 
-
 migration = pd.read_csv(r'C:\Users\Bridg\Desktop\CIDA\pythonProjectFDI\API_Edit.csv')
 
 engine = create_engine('sqlite:///:memory:')
@@ -17,19 +16,11 @@ migration.to_sql('data_table', engine)
 
 migration = migration.loc[:, ['Country Name', 'Country Code', '1962', '1972', '1977', '1982', '1987', '1992', '1997', '2002', '2007', '2012', '2017']]
 
-#print(migration.head())
-
 fdi_migration = fdi_csv.merge(migration, on= ['Country Name', 'Country Code'], how='left')
 
 print(fdi_migration.head())
+print(migration.head())
 
-
-#fdi_migration.groupby('Country Code').agg(' ')
-
-#print(fdi_migration)
-
-
-
-
+print(fdi_migration)
 
 
